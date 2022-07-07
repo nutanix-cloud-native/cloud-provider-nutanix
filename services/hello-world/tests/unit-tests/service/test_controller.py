3f77994c1c1bd0805ce7133a4929ae539ef1370b
Andy Daniel <andy.daniel@nutanix.com>
2022-07-07 15:15:45 -0400

    Initial commit.

--- services/hello-world/tests/unit-tests/service/test_controller.py
+++
@@ -1,8 +0,0 @@
-from service import controller
-
-def test_health_check(mocker):
-    mocker.patch("service.controller", "app")
-
-    assert controller.hello_world() == '{"message": "hello-world"}'
-
-
